bf75e862fe97857fecae29f51ff932c257321c74
kralos-7 <c_rojasx@hotmail.com>
2024-01-16 10:29:28 -0600

    add pytest

---
+++ code/pytest/example_0/src/login.py
@@ -0,0 +1,5 @@
+def login(user, password):
+    if((user == "umarino") and (password == "2019-2024")):
+        return True
+    else:
+        return False
---
+++ code/pytest/example_0/src/models/language_model.py
@@ -0,0 +1,6 @@
+class language_model():
+
+    @classmethod
+    def get_languages(cls):        
+        languages = ["c","ensamblador","java","python","javascript"]
+        return languages
